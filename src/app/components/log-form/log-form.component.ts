7528f4a6ddec8ded42695eb40d4e028d1297f444
Josh <jlockrow@gmail.com>
2019-01-09 10:13:10 -0600

    brings in a id generator from stackexchange

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -27,6 +27,16 @@ export class LogFormComponent implements OnInit {
     });
   }
   onSubmit(log: Log){
-    console.log('button clicked');
+    const newLog = {
+
+    }
   }
+  
+  generateId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+  
 }
