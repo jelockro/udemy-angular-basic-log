4b6086e2802dfbb86994cc1e57c49affcbeae7d8
Josh <jlockrow@gmail.com>
2019-01-08 23:55:55 -0600

    serves log elements from logs.component

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
+  logs: {
+    id: string,
+    text: string,
+    date: any
+  }[];
 
   constructor() { }
 
   ngOnInit() {
+    this.logs = [
+      {id: '1', text: 'Generated Components', date: new Date('01/08/2018 12:54:27')},
+      {id: '2', text: 'Adds Bootstrap', date: new Date('01/08/2018 12:54:27')},
+      {id: '3', text: 'adds more logs', date: new Date('01/08/2018 12:54:27')}
+    ]
   }
 
 }
