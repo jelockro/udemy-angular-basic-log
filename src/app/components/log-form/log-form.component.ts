c8ab9454f15df5fcd032c49ab8fe2517027532df
Josh <jlockrow@gmail.com>
2019-01-09 10:10:07 -0600

    creates onSubmit function

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -11,15 +11,22 @@ export class LogFormComponent implements OnInit {
   id: string;
   text: string;
   date: any;
+
+  isNew: boolean = true;
+
   constructor(private _logService: LogService) { }
 
   ngOnInit() {
     this._logService.selectedLog.subscribe(log => {
       if (log.id !== null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
     });
+  }
+  onSubmit(log: Log){
+    console.log('button clicked');
   }
 }
